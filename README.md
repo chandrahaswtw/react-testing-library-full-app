c6dd6049ecce9349a9d7cdd63657b2182226b8d0
Chandrahas Balleda <chandrahas.balleda@perficient.com>
2023-08-11 00:08:53 +0530

    Note on MSW

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ NOTE: This project was bootstrapped with [Create React App](https://github.com/f
 ### `npm test`
 
 All the test files are places inside the **src/\_\_tests\_\_** folder. Run `npm test` to run all the tests.
-
-# About the app
--- src/test/server.js
+++ src/test/server.js
@@ -23,37 +23,46 @@ export const createServer = (configs) => {
   afterAll(() => server.close());
 };
 
-// NOTE: The below is the actual code, we made below code reusable as above.
-// import { rest } from "msw";
-// import { setupServer } from "msw/node";
-// const handlers = [
-//     // If URL orignated from same port, just use /users. Since external URL, use full URL
-//     rest.get("https://jsonplaceholder.typicode.com/users", (req, res, ctx) => {
-//       return res(
-//         ctx.json([
-//           {
-//             id: "43",
-//             name: "Chandrahas Balleda",
-//             email: "chandrahaswtw@gmail.com",
-//           },
-//           {
-//             id: "44",
-//             name: "Vikram seth",
-//             email: "vikram.seth@gmail.com",
-//           },
-//         ])
-//       );
-//     }),
-//   ];
+// NOTE 1:
+/*
+If we wish to add more than one `createServer` within same test file, wrap within describe so that those tests written in a describe will use the server. If we directly specifies `jest` executes all the code other than tests and executes all the test cases at once. We cannot limit a server for particular tests.
+The before* after* hooks will limit to the particular `describe` as well so, use `describe` to compartmentalize the code.
+*/
 
-//   const server = setupServer(...handlers);
+// NOTE: 2
+/*
+The below is the actual code, we made below code reusable as above.
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+const handlers = [
+    // If URL orignated from same port, just use /users. Since external URL, use full URL
+    rest.get("https://jsonplaceholder.typicode.com/users", (req, res, ctx) => {
+      return res(
+        ctx.json([
+          {
+            id: "43",
+            name: "Chandrahas Balleda",
+            email: "chandrahaswtw@gmail.com",
+          },
+          {
+            id: "44",
+            name: "Vikram seth",
+            email: "vikram.seth@gmail.com",
+          },
+        ])
+      );
+    }),
+  ];
 
-//   // Enable request interception.
-//   beforeAll(() => server.listen());
+  const server = setupServer(...handlers);
 
-//   // Reset handlers so that each test could alter them
-//   // without affecting other, unrelated tests.
-//   afterEach(() => server.resetHandlers());
+  // Enable request interception.
+  beforeAll(() => server.listen());
 
-//   // Don't forget to clean up afterwards.
-//   afterAll(() => server.close());
+  // Reset handlers so that each test could alter them
+  // without affecting other, unrelated tests.
+  afterEach(() => server.resetHandlers());
+
+  // Don't forget to clean up afterwards.
+  afterAll(() => server.close());
+*/
